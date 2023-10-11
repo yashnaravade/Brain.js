ff41ebdc82d8947d05c1df7509c1b6028e1ef42d
Yash <yashdip123@gmail.com>
2023-10-11 14:24:29 +0530

    Add output

--- index.js
+++ index.js
@@ -10,12 +10,17 @@ network.train([
   { input: [1, 1], output: [0] },
 ]);
 
-const output = network.run([1, 0]); // [0.987]
-const output2 = network.run([0, 0]); // [0.987]
-const output3 = network.run([0, 1]); // [0.987]
-const output4 = network.run([1, 1]); // [0.987]
+const output = network.run([1, 0]); 
+const output2 = network.run([0, 0]);
+const output3 = network.run([0, 1]); 
+const output4 = network.run([1, 1]);
 
 console.log(output);
 console.log(output2);
 console.log(output3);
 console.log(output4);
+
+// Float32Array(1) [ 0.9333660006523132 ]
+// Float32Array(1) [ 0.04921703785657883 ]
+// Float32Array(1) [ 0.9342756271362305 ]
+// Float32Array(1) [ 0.09326431900262833 ]
